4da9b87244b9d74c7383926eeec6ca196ddc35d8
Jones <adam.jones@nordicsemi.no>
2023-04-17 14:26:06 -0700

    Reverting

--- src/js/components/DataTypes/DataTypeLabel.js
+++ src/js/components/DataTypes/DataTypeLabel.js
@@ -5,8 +5,8 @@ import Theme from './../../themes/getStyle';
 
 export default class extends React.PureComponent {
     render() {
-        const { rjvId, type_name, displayDataTypes, theme, editModee } = this.props;
-        if (editMode) {
+        const { rjvId, type_name, displayDataTypes, theme } = this.props;
+        if (displayDataTypes) {
             return (
                 <span
                     class="data-type-label"
