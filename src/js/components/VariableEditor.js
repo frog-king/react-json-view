182c15691c1232d664a470d75680d916574cfee3
Jones <adam.jones@nordicsemi.no>
2023-04-18 10:21:26 -0700

    adding default value

--- src/js/components/VariableEditor.js
+++ src/js/components/VariableEditor.js
@@ -300,8 +300,8 @@ class VariableEditor extends React.PureComponent {
                 />
 
                 <select id="type">
-                    <option value="boolean">Boolean</option>
-                    <option value="string">String</option>
+                    <option value="boolean" {...t === "boolean" && selected}>Boolean</option>
+                    <option value="string" {...t === "string" && selected}>String</option>
                     <option value="integer">Integer</option>
                     <option value="float">Float</option>
                     <option value="date">Date</option>
